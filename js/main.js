bff59fc616da806823c256c46248517c7d23596f
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-08 23:33:19 +0200

    Edit js file
    add some logic to navbar.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ let linksDiv = document.querySelector(".header .links");
 bars.onclick = function () {
   linksDiv.classList.toggle("open-links");
 };
+
+let links = document.querySelectorAll(".links li");
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    e.target.classList.add("active");
+  });
+});
