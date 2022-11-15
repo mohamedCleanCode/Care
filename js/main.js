0c6ce3d2077d246b17a81afbc2e92e35bb40092d
Mohamed Mostafa <66911335+mohamedCleanCode@users.noreply.github.com>
2022-11-15 23:11:30 +0200

    Edit js file
    add logic to product details page and shop page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// [1] Logic Of Navbar
+// [1] Logic Of Index Page
 let bars = document.querySelector(".bars");
 let linksDiv = document.querySelector(".header .links");
 
@@ -15,3 +15,21 @@ links.forEach((link) => {
     e.target.classList.add("active");
   });
 });
+
+// Logic Of Shop Page
+let productImage = document.querySelectorAll(".product-shop");
+productImage.forEach((product) => {
+  product.addEventListener("click", (e) => {
+    window.location.href = "product-details.html";
+  });
+});
+
+// Logic Of Details Page
+let mainImage = document.querySelector(".main-image");
+let smallImgs = document.querySelectorAll(".small-image img");
+smallImgs.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    mainImage.src = e.target.src;
+  });
+});
+
